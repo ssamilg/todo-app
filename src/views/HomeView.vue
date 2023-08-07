0a0f62f3d5bf11a17f148cad9f51df00e702658b
ssamilg <samil-98@hotmail.com>
2023-08-07 20:17:11 +0300

    redesign login page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="home-view">
+    <h1 class="text-3xl font-bold underline">
+      Hello there !
+    </h1>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+#home-view {
+  height: 100%;
+}
+</style>
